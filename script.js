563bdb4a3bc32d4e0e21ec621b7644bdc8c7593c
darshikaguptaa <dng2804@gmail.com>
2022-01-11 13:44:30 +0530

    JS added

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ let newButton = document.getElementById("new-book");
 let formBox = document.getElementById("container");
 let submitButton = document.getElementById("submit");
 let bookDiv = document.getElementById("books");
-// let nameInput = getElementById("name"); 
-// let authorInput = getElementById("author");
-// let pagesInput = getElementById("pages");
-// let readInput = getElementById("name");
+let myForm = document.getElementById("my-form");
+
 let formOpen = false;
 
 function newBook(name,author,pages)
@@ -13,44 +11,9 @@ function newBook(name,author,pages)
     this.name = name;
     this.author= author;
     this.pages = pages;
-    // this.read = read
 }
 
-let books = [
-    {
-        name : "Harry Potter",
-        author : "J.K.Rowling",
-        pages : 500,
-        read : "Yes"
-    },
-    {
-        name : "Harry Potter",
-        author : "J.K.Rowling",
-        pages : 500,
-        read : "Yes"
-    }
-]
-
-
-
-books.forEach((book=>{
-    let nameNode = document.createElement('div');
-    nameNode.classList.add("book");
-    nameNode.innerText = `Name = ${book.name}`
-    bookDiv.appendChild(nameNode);
-
-    let authorNode = document.createElement('h2');
-    authorNode.innerText = `Author = ${book.author}`
-    nameNode.appendChild(authorNode)
-
-    let pageNode = document.createElement('h2');
-    pageNode.innerText = `Number of pages = ${book.pages}`
-    nameNode.appendChild(pageNode)
-
-    let readNode = document.createElement('h2');
-    readNode.innerText = `Read = ${book.read}`
-    nameNode.appendChild(readNode)
-}))
+let books = []
 
 newButton.addEventListener("click",()=>{
     if(formOpen===false)
@@ -60,9 +23,53 @@ newButton.addEventListener("click",()=>{
     }
 })
 
-submitButton.addEventListener("click",()=>{
+submitButton.addEventListener("click",(event)=>{ 
+    event.preventDefault()
+
+    let nameInput = document.getElementById("name").value;
+    let authorInput = document.getElementById("author").value;
+    let pagesInput = document.getElementById("pages").value;
+    
+    let x = new newBook(nameInput,authorInput,pagesInput);
+    books.push(x);
+    console.log(books);
+    
+    let nameNode = document.createElement('div');
+    nameNode.classList.add("book");
+    nameNode.innerText = `Name = ${x.name}`
+    bookDiv.appendChild(nameNode);
+    
+    let authorNode = document.createElement('h2');
+    authorNode.innerText = `Author = ${x.author}`
+    nameNode.appendChild(authorNode)
+    
+    let pageNode = document.createElement('h2');
+    pageNode.innerText = `Number of pages = ${x.pages}`
+    nameNode.appendChild(pageNode)
+    
+    var readButton = document.createElement("button");
+    readButton.innerText = "read"
+    nameNode.appendChild(readButton)
+
+    readButton.addEventListener("click", ()=>{
+        if (readButton.innerText == "read"){
+            readButton.innerText = "not read"
+        }
+        else if(readButton.innerText == "not read"){
+            readButton.innerText = "read"
+        }
+    })
+    // let readNode = document.createElement('h2');
+    // readNode.innerText = `Read = ${x.read}`
+    // nameNode.appendChild(readNode)
+
+
     formBox.style.transform ="scale(0)";
     formOpen = false;
+
 })
+
+
+
 
 
