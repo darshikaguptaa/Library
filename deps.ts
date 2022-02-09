3c057ae76716cc4abffeef6bb9373e39330c57c9
Heikrana <vaibhavsoni@duck.com>
2022-02-10 00:28:16 +0530

    Add Server to serve static files from ./Client/

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { serve } from "https://deno.land/std@0.125.0/http/server.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.125.0/streams/mod.ts";
---
+++ server.ts
@@ -0,0 +1,51 @@
+import { serve, readableStreamFromReader } from "./deps.ts";
+
+async function requestHandler(req: Request): Promise<Response> {
+	const url = new URL(req.url);
+	if (req.method === "GET") {
+		if (url.pathname === "/") {
+			try {
+				const file = await Deno.open("./Client/index.html", { read: true });
+				const readableStream = readableStreamFromReader(file);
+				return new Response(readableStream, {
+					status: 200,
+					headers: { "content-type": "text/html" },
+				});
+			} catch (err) {
+				console.error(err);
+				return new Response("index.html not found", { status: 404 });
+			}
+		} else if (url.pathname.startsWith("/style.css")) {
+			try {
+				const file = await Deno.open("./Client/style.css", { read: true });
+				const readableStream = readableStreamFromReader(file);
+				return new Response(readableStream, {
+					status: 200,
+					headers: { "content-type": "text/css" },
+				});
+			} catch (err) {
+				console.error(err);
+				return new Response("style.css not found", { status: 404 });
+			}
+		} else if (url.pathname.startsWith("/script.js")) {
+			try {
+				const file = await Deno.open("./Client/script.js", { read: true });
+				const readableStream = readableStreamFromReader(file);
+				return new Response(readableStream, {
+					status: 200,
+					headers: { "content-type": "text/javascript" },
+				});
+			} catch (err) {
+				console.error(err);
+				return new Response("script.js not found", { status: 404 });
+			}
+		}
+	}
+	return new Response("Henlo", {
+		status: 200,
+		headers: { "content-type": "text-plain; charset=utf-8" },
+	});
+}
+
+console.log("Listening on localhost:3000");
+serve(requestHandler, { port: 3000 });
