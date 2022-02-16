e2c490f68509084d13a1036ff60146489b18f30b
darshikaguptaa <dng2804@gmail.com>
2022-02-16 11:50:14 +0530

    Added Local Storage

--- Client/script.js
+++ Client/script.js
@@ -16,6 +16,7 @@ function newBook(name,author,pages)
 
 let books = []
 
+
 newButton.addEventListener("click",()=>{
     if(formOpen===false)
     {
@@ -26,63 +27,70 @@ newButton.addEventListener("click",()=>{
 
 submitButton.addEventListener('click',(event)=>{ 
 
-
+    
     event.preventDefault()
-
+    
     let nameInput = document.getElementById("name").value;
     let authorInput = document.getElementById("author").value;
     let pagesInput = document.getElementById("pages").value;
     
     let x = new newBook(nameInput,authorInput,pagesInput);
+    console.log(x)
     books.push(x);
     console.log(books);
-    
 
+    localStorage.setItem("Book",JSON.stringify(books)); 
+    addOne(x)
+
+    formBox.style.transform ="scale(0)";
+    formOpen = false;
+    
+})
+closeButton.addEventListener("click",(event)=>{
+    
+    event.preventDefault()
+    if(formOpen)
+    {
+        formBox.style.transform ="scale(0)"
+        formOpen =  false;
+        return
+    }
+    
+})
+
+if (books.length==0){
+    if (localStorage.length == 0){
+        books = []
+    }
+    else{
+        books = JSON.parse(localStorage.getItem("Book"))
+    }
+    add()
+}
+
+function add(){
+    for (const obj of books) {
+        console.log(obj)
+        addOne(obj)
+    }
+}
+
+
+
+function addOne(obj){
     let nameNode = document.createElement('div');
     nameNode.classList.add("book");
-    nameNode.innerText = `Name = ${x.name}`
+    nameNode.textContent = `Name = ${obj.name}`
     bookDiv.appendChild(nameNode);
     
     let authorNode = document.createElement('p');
-    authorNode.innerText = `Author = ${x.author}`
+    authorNode.textContent = `Author = ${obj.author}`
     nameNode.appendChild(authorNode)
     
     let pageNode = document.createElement('p');
-    pageNode.innerText = `No. of pages = ${x.pages}`
+    pageNode.textContent = `No. of pages = ${obj.pages}`
     nameNode.appendChild(pageNode)
-    
-    var readButton = document.createElement("button");
-    readButton.innerText = "Read"
-    readButton.classList.add("read")
-    nameNode.appendChild(readButton)
-
-    readButton.addEventListener("click", ()=>{
-        if (readButton.innerText == "Read"){
-            readButton.innerText = "Not Read"
-        }
-        else if(readButton.innerText == "Not Read"){
-            readButton.innerText = "Read"
-        }
-    })
-    formBox.style.transform ="scale(0)";
-    formOpen = false;
-
-    closeButton.addEventListener("click",(event)=>{
-
-        event.preventDefault()
-        if(formOpen)
-        {
-            formBox.style.transform =" scale(0)"
-            formOpen =  false;
-            return
-        }
-
-    })
-
-    // myForm.reset();
-
-})
-
+}
 
 
 
